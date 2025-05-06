4690a82ce5296b11fb47cfd5544bf58133f5a873
krithikahkotian <krithikah.cs23@bmsce.ac.in>
2025-05-06 09:47:17 +0530

    Create NQueen.c

---
+++ NQueen.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+int x[20], count = 1;
+
+void queens(int k, int n);
+int place(int k, int j);
+
+int main() {
+    int n;
+
+    printf("\nEnter the number of queens to be placed: ");
+    scanf("%d", &n);
+
+    queens(1, n);
+    return 0;
+}
+
+void queens(int k, int n) {
+    int j;
+    for (j = 1; j <= n; j++) {
+        if (place(k, j)) {
+            x[k] = j;
+            if (k == n) {
+                printf("\nSolution %d:\n", count++);
+                for (int i = 1; i <= n; i++) {
+                    printf("%d row <--- %d column\n", i, x[i]);
+                }
+            } else {
+                queens(k + 1, n);
+            }
+        }
+    }
+}
+
+int place(int k, int j) {
+    for (int i = 1; i < k; i++) {
+        if (x[i] == j || abs(x[i] - j) == abs(i - k)) {
+            return 0;
+        }
+    }
+    return 1;
+}
